63a4a6bfc81ba9fa502d2d17b090f1c5247bca16
industriasorgatek-debug <industrias.orgatek@gmail.com>
2026-07-26 19:08:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -386,7 +386,7 @@ else:
         
             if df.empty:
                 st.info("No hay embarques registrados aún.")
-                else:
+            else:
             invoices_con_pago_ff = df_pagos_all[df_pagos_all['tipo_pago'] == 'Pago a Freight Forwarder']['num_invoice'].unique() if not df_pagos_all.empty else []
 
             def check_pago_ff(row):
