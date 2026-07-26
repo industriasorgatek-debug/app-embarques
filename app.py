cb7627ea04f2fbcc1cb357c60eeddb2a611e032d
industriasorgatek-debug <industrias.orgatek@gmail.com>
2026-07-26 19:50:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -399,8 +399,8 @@ else:
     # Convertimos a minúsculas y limpiamos espacios para que funcione aunque cambien mayúsculas/minúsculas
         val_str = str(val).lower().strip()
     
-    if "entregado" in val_str:
-    return 'background-color: #F8D7DA; color: #721C24; font-weight: bold;' # Rojo (Alerta/Finalizado)
+        if "entregado" in val_str:
+        return 'background-color: #F8D7DA; color: #721C24; font-weight: bold;' # Rojo (Alerta/Finalizado)
     
     elif "pendiente por pagar" in val_str:
         return 'background-color: #FFF3CD; color: #856404; font-weight: bold;' # Amarillo Ámbar (Precaución)
