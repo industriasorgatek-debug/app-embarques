34e247ff958e005eaaec8b5f680fbe7e3ddfc7d6
industriasorgatek-debug <industrias.orgatek@gmail.com>
2026-07-26 21:18:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -430,8 +430,8 @@ def check_pago_ff(row):
         df['pago_flete_status'] = df.apply(check_pago_ff, axis=1)
 
     # --- 3. PREPARACIÓN DE VISUALIZACIÓN ---
-            base_cols = ['num_invoice', 'num_bl', 'naviera', 'fabricante', 'producto', 'origen', 'destino', 'eta', 'estatus']
-            base_names = ['N° Invoice', 'N° BL', 'Línea Naviera', 'Fabricante', 'Producto', 'Origen', 'Destino', 'ETA (Arribo)', 'Estatus']
+        base_cols = ['num_invoice', 'num_bl', 'naviera', 'fabricante', 'producto', 'origen', 'destino', 'eta', 'estatus']
+        base_names = ['N° Invoice', 'N° BL', 'Línea Naviera', 'Fabricante', 'Producto', 'Origen', 'Destino', 'ETA (Arribo)', 'Estatus']
 
         if role == "admin":
         base_cols += ['pago_flete_status', 'alerta_exoneracion']
