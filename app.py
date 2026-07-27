601b2a49d4e8eec659bf98504470bbfc49ffc960
industriasorgatek-debug <industrias.orgatek@gmail.com>
2026-07-26 20:59:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -360,7 +360,7 @@ else:
     st.title("🚢 Control de Embarques")
        
     # --- 1. CARGA DE DATOS ---
-df = pd.read_sql_query("SELECT * FROM embarques",conn)
+df = pd.read_sql_query("SELECT * FROM embarques", conn)
 df_pagos_all = pd.read_sql_query("SELECT num_invoice, tipo_pago FROM pagos_embarques", conn)
     
     # --- 2. LÓGICA DE ALERTAS Y COLORES ---
